c5f36e487323f78d206b35f363a08250d41878c3
hananalrahma <iqbal.alrahma@gmail.com>
2023-06-08 20:28:41 +0700

    commit

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Ngoding\Python\Evacuation route gedung DC Q-learning
+set VIRTUAL_ENV=D:\Ngoding\Python\Evacuation-route-Q-learning-gedung-DC
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(Evacuation route gedung DC Q-learning) %PROMPT%
+set PROMPT=(Evacuation-route-Q-learning-gedung-DC) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
